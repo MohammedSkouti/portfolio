2013fe25784370679a2047872e4a23e0c0cb18e7
MohammedSkouti <121097406+MohammedSkouti@users.noreply.github.com>
2023-07-31 22:05:18 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,6 +58,7 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.education__data, .work__img, .work__container',{interval: 200}); 
 sr.reveal('.education__data, .work__img, .contact__input',{interval: 200}); 
 <!--===== JS FOR COUNTER =====-->
 
